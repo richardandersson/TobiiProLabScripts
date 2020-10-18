281c355812852ba44367d58a47455b44e948112e
Richard Andersson <richard.andersson@tobii.com>
2020-10-18 16:02:18 +0200

    Updated encoding handling and long recording plotting
    Updated to work with later versions of Pro Lab ( >= 1.138). Also fixed a bug on timestamps, causing data to not display for data longer than just a couple of seconds.

--- floating bar graph/floating_bar_graph.py
+++ floating bar graph/floating_bar_graph.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec  3 11:31:04 2018
-Plotting AOI over time from Lab data export
+Plotting AOI over time from Pro Lab data export
 @author: Richard Andersson @ Tobii Pro
 
 Requires the Python package Matplotlib
 (Ought to be part of any decent scientific Python installation)
 
-Plots fixations and not visits/dwells.
+Plots by fixation and not visits/dwells.
 Handles overlapping AOIs.
+
+Limitations:
+Only for one participant and not many. (the fixation times will overlap and 
+just create long streaks for the AOIs and not the transition patterns we want)
 
 No warranties or guarantees!
 
@@ -17,7 +21,7 @@ No warranties or guarantees!
 
 
 # POINT FILENAME TO YOUR EXPORTED DATA FILE HERE
-filename = r'data export.tsv' # Add path as needed.
+filename = r'Data Export.tsv' # Add path as needed.
 ###############################################################################
 
 # EXTRACT FIXATIONS AND AOI HITS FROM FILE
@@ -27,8 +31,8 @@ start_list = []
 dur_list = []
 name_list = []
 
-
-with open(filename, mode='r', encoding='UTF-8-sig') as x:
+# Change encoding to 'utf-8-sig' for Pro Lab versions before 1.138
+with open(filename, mode='r', encoding='utf-8') as x:
     dat = x.readlines()
 
 
@@ -49,27 +53,32 @@ try:
     for j in range(len(aoii)):
         aoicolnames.append( re.findall('AOI hit \[.+ - (.+)\]', elems[aoii[j]])[0] )
     
+    if len(aoicolnames) < 1:
+        raise Exception('No AOI columns part of data file!')
+    
 except:
     print("Being able to locate the correct columns is a requirement!")
     raise
-    
 
 
+# SETTING TIME START
+start_time = None
+for i in range(1,len(dat)):
+    elems = dat[i].strip().split('\t')
+    if start_time is None and elems[ti] != '':
+        start_time = int(elems[ti])
+        break
 
 
+# PROCESS FIXATION DATA
 # for every aoi column (we process separately b/c not necessarily mutually exclusive)
 for k in range(len(aoii)):
     current_em = None
     previous_em = None
-    start_time = None
     
     for i in range(1,len(dat)):
         elems = dat[i].strip().split('\t')
         
-        # If we have no start time of this recording yet, set this first
-        if start_time is None and elems[ti] != '':
-            start_time = elems[ti]
-
         if elems[eti] == 'Fixation' and elems[ti] != '':
             
             current_em = elems[emi] # update eye movement index
@@ -85,7 +94,7 @@ for k in range(len(aoii)):
                     continue
                 
                 if hit == '1': # a new fixation and is an AOI-hit
-                    start_list.append( int(elems[ti]) - int(start_time) )
+                    start_list.append( (int(elems[ti]) - start_time)/1000 )
                     dur_list.append( float(elems[di]) )
                     name_list.append( float(k) )
                     previous_em = current_em
@@ -97,7 +106,6 @@ for k in range(len(aoii)):
 
 
 # PLOTTING THE DATA
-
 import matplotlib.pyplot as plt
 
 plt.barh(y=name_list, 
@@ -108,7 +116,6 @@ plt.barh(y=name_list,
 ax = plt.gca()
 ax.set_yticks(list(range(len(aoii))))
 ax.set_yticklabels(aoicolnames)
-plt.xlabel('Recording time')
+plt.xlabel('Time (ms from start)')
 plt.ylabel('Areas of interest')
-
 plt.show()
